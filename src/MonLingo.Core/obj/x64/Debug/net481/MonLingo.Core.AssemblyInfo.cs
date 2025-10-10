47214231b3616ed86f0e46b3045800b2ce8a3e71
panpan9121 <panpan9121@gmail.com>
2025-10-10 21:20:01 +0800

    備份: 階段三段落合併算法 (v3) - 準備移除並使用AI翻譯替代

--- src/MonLingo.Core/obj/x64/Debug/net481/MonLingo.Core.AssemblyInfo.cs
+++ src/MonLingo.Core/obj/x64/Debug/net481/MonLingo.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe6cd0116aee3841926a6f486664c93880e1c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82337d4e057ec9575585884699e945165b797004")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonLingo.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonLingo Core Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
